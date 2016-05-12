a29ed0ece5179ae8f2be1648b7ef24369af854b8
ziwei <ziwei@gamepoch.com>
2016-05-12 13:06:26 +0800

    Fix bugs

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var http = require('http');
 var connect = require('connect');
 var config = require('./config');
 var mp = require('./controllers/wechat_mp');
-var corp = require('wechat-enterprise');
+//var corp = require('wechat-enterprise');
 
 var app = connect();
 connect.logger.format('home', ':remote-addr :response-time - [:date] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent" :res[content-length]');
@@ -20,10 +20,10 @@ app.use('/wechat/callback', mp.callback);
 app.use('/wechat', mp.reply);
 app.use('/detail', mp.detail);
 app.use('/login', mp.login);
-app.use('/corp', corp(config.corp, function (req, res, next) {
-  res.writeHead(200);
-  res.end('hello node api');
-}));
+//app.use('/corp', corp(config.corp, function (req, res, next) {
+//  res.writeHead(200);
+//  res.end('hello node api');
+//}));
 
 app.use('/', function (req, res) {
   res.writeHead(200);
@@ -45,3 +45,5 @@ var worker = require('pm').createWorker();
 worker.ready(function (socket) {
   server.emit('connection', socket);
 });
+
+app.listen(80);
